da7e0bd62b530ac7cb331b9347cb8b9fafeb4668
JAJAEC <jajaec1025@gmail.com>
2022-09-21 08:01:09 +0900

    db2 테스트코드 추가

---
+++ db2_test.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  5 08:17:43 2019
+
+@author: XKJR
+"""
+
+#import ibm_db
+
+#, "cspro", "cspro
+#pconn = ibm_db.pconnect("as400.iroyal.kr", "cspro", "cspro")
+
+#conn = ibm_db.connect("DATABASE=SALPGM3;HOSTNAME=as400.iroyal.kr;PORT=8471;PROTOCOL=TCPIP;UID=cspro;PWD=cspro;", "", "")
+#ibm_db.connect("Server=as400.iroyal.kr:60000;Database=salpgm3;UID=cspro;PWD=cspro;", "", "")
+#pconn = ibm_db.pconnect("as400.iroyal.kr", "cspro", "cspro")
+
+import pyodbc
+
+connection = pyodbc.connect(driver='{iSeries Access ODBC Driver}', system='as400', uid='cspro', pwd='cspro')
+
+c1 = connection.cursor()
+
+c1.execute('select * from SELPGM3.B2CMBR')
+
+for row in c1:
+	print(row)
+
+connection.close()
